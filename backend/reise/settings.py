7a96ade4b3a3e4d59537e6966ce8c52841be1220
praktikant101 <ek27global312@gmail.com>
2023-05-06 17:09:54 +0600

    nginx 3.0

--- backend/reise/settings.py
+++ backend/reise/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config('DEBUG', cast=bool, default=False)
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
--- backend/reise/urls.py
+++ backend/reise/urls.py
@@ -37,7 +37,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('post/', include('apps.urls')),
-    path('admin/', admin.site.urls),
+    path('adminpage/', admin.site.urls),
 ]
 
 if settings.DEBUG:
